14c29e3b1fdfa18de2c6bca8f670fe8e958d8989
Brandon Satrom <bsatrom@gmail.com>
2015-01-30 22:18:40 -0600

    added piezo buzzer

---
+++ piezoBuzzer/piezoBuzzer.ino
@@ -0,0 +1,44 @@
+const int buzzerPin = 9;
+const int songLength = 18;
+
+char notes[] = "cdfda ag cdfdg gf "; // a space represents a rest.
+int beats[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
+
+int tempo = 150;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  int i, duration;
+  
+  for(i = 0; i < songLength; i++) {
+    duration = beats[i] * tempo;
+    
+    if (notes[i] == ' ') {
+      delay(duration);
+    } else {
+      tone(buzzerPin, frequency(notes[i]), duration);
+      delay(duration);
+    }
+    delay(tempo/10);
+  }
+  
+  // while(true){} // Loop when you only want to play this once
+}
+
+int frequency(char note) {
+  int i;
+  const int numNotes = 8;
+  
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  
+  for (i = 0; i < numNotes; i++) {
+    if (names[i] == note) {
+      return(frequencies[i]);
+    }
+  }
+  return(0);
+}
